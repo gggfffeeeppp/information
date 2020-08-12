3231cd83212e075cd3d6895be5cd732f64bdc411
gggfffeeeppp <549819224@qq.com>
2020-08-12 09:32:14 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * FileName: HelloWorld
+ * Author:   蔡志勇
+ * Date:     2020/8/11 16:19
+ * Description:
+ * History:
+ */
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ * @author 蔡志勇
+ * @date 2020/8/11
+ * @version 1.0.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
